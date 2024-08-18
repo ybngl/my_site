04979e53654661d5f3314b0104ea9818d26f436a
ybngl <akbabas125@gmail.com>
2024-08-18 19:02:03 +0300

    Initial commit for my_site project

---
+++ blogapp/blog/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.shortcuts import HttpResponse
+from django.shortcuts import render
+from blog.models import Blog, Category
+
+# Create your views here.
+def index(request):
+    context = {
+        "blogs": Blog.objects.filter(is_active=True, is_home=True),
+        "categories": Category.objects.all()
+    }
+    return render(request, "blog/index.html", context)
+
+def blogs(request):
+    context = {
+        "blogs": Blog.objects.filter(is_active=True),
+        "categories": Category.objects.all()
+    }
+    return render(request, "blog/blogs.html", context)
+
+def blog_details(request, slug):
+   blog = Blog.objects.get(slug=slug)
+   return render(request, "blog/blog-details.html", {
+        "blog": blog
+    })
+
+def blogs_by_category(request, slug):
+    context = {
+        "blogs": Category.objects.get(slug=slug).blog_set.filter(is_active=True),
+        "categories": Category.objects.all(),
+        "selected_category": slug
+    }
+    return render(request, "blog/blogs.html", context)
